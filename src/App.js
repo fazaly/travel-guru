f1c701cc7079a79f32cae078cfa9495c6c4f1940
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-09-29 16:26:16 +0600

    react question & answer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
   return (
     <div className="App">
         <Travel></Travel>
-        
+        <h1 className='question-title'>Important Question and Answer About React</h1>
+        <div className='ans-cart'>
+            <h2>1. How does react work?</h2>
+            <p>React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
+        </div>
+        <div className='ans-cart'>
+            <h2>2. What are the difference between Props and State?</h2>
+            <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
+            </div>
+        <div className='ans-cart'>
+            <h2>1. How does react work?</h2>
+            <p>React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
+        </div>
     </div>
   );
 }
